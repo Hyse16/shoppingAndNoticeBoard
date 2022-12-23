797d3291f46103438bee92888f83e1105283c42d
Hyse16 <gustmd4296@hanmail.net>
2022-12-23 20:30:57 +0900

    Initial commit

---
+++ src/main/java/com/project/shoppingAndNoticeBoard/ShoppingAndNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.project.shoppingAndNoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAndNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAndNoticeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/shoppingAndNoticeBoard/ShoppingAndNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shoppingAndNoticeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAndNoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
