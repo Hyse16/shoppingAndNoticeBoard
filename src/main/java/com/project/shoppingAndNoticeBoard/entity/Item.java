656c0a526cb28d97d99f80ff8fc4ae76404325cc
Hyse16 <gustmd4296@hanmail.net>
2022-12-27 19:14:33 +0900

    Item엔티티, ItemSellStatus 추가

---
+++ src/main/java/com/project/shoppingAndNoticeBoard/entity/Item.java
@@ -0,0 +1,49 @@
+package com.project.shoppingAndNoticeBoard.entity;
+
+import com.project.shoppingAndNoticeBoard.constant.ItemSellStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+    @Builder
+    public Item(String itemNm, int price, int stockNumber, String itemDetail, ItemSellStatus itemSellStatus) {
+        this.itemNm = itemNm;
+        this.price = price;
+        this.stockNumber = stockNumber;
+        this.itemDetail = itemDetail;
+        this.itemSellStatus = itemSellStatus;
+    }
+
+
+}
