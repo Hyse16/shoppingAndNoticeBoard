365c64e7427ba27138d43ced4c10b67ff6760be3
Hyse16 <gustmd4296@hanmail.net>
2023-01-09 17:16:03 +0900

    카카오 로그인 기능 구현

--- src/main/java/com/project/shoppingAndNoticeBoard/Config/SecurityConfig.java
+++ src/main/java/com/project/shoppingAndNoticeBoard/Config/SecurityConfig.java
@@ -45,6 +45,11 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
 
 
--- src/main/java/com/project/shoppingAndNoticeBoard/Controller/MemberController.java
+++ src/main/java/com/project/shoppingAndNoticeBoard/Controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.project.shoppingAndNoticeBoard.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.shoppingAndNoticeBoard.Dto.MemberFormDto;
 import com.project.shoppingAndNoticeBoard.Service.MemberService;
 import com.project.shoppingAndNoticeBoard.entity.Member;
+import com.project.shoppingAndNoticeBoard.entity.OAuthToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -22,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 
+@RequestMapping(value = "/members")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -29,13 +33,13 @@ public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
-    @GetMapping(value = "/members/new")
+    @GetMapping(value = "/new")
     public String memberForm(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
 
-    @PostMapping(value = "/members/new")
+    @PostMapping(value = "/new")
     public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
@@ -53,44 +57,16 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @GetMapping(value = "/members/login")
+    @GetMapping(value = "/login")
     public String loginMember() {
         return "/member/memberLoginForm";
     }
 
-    @GetMapping(value = "/members/login/error")
+    @GetMapping(value = "/login/error")
     public String loginError(Model model) {
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
         return "/member/memberLoginForm";
     }
 
-    @GetMapping("/auth/kakao/callback")
-    public @ResponseBody String authLogin(String code) {
 
-        //HttpHeader 오브젝트 생성
-        RestTemplate rt = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        //HttpBody 오브젝트 생성
-        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type","authorization_code");
-        params.add("client_id","dd1f34327fe4c230bae90b6a4b9d0b11");
-        params.add("redirect_uri","http://localhost:8080/auth/kakao/callback");
-        params.add("code",code);
-
-        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
-
-        // Http 요청하기 - Post방식으로 - 그리고 response  변수의 응답을 받음
-        ResponseEntity<String> response = rt.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                String.class
-        );
-
-
-        return "카카오 토큰 요청 완료 : 토큰요청에 대한 응답 :" + response;
-    }
 }
